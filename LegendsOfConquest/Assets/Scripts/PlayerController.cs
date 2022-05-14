6a1f5b628b5e36a5c7d7d9260e425e4c7503d19a
catherine stafford <catstaffo@gmail.com>
2022-05-14 18:02:23 -0400

    Initial
    --Player animation
    --Player input movement
    --Blend tree

---
+++ LegendsOfConquest/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D playerRigidbody;
+    [SerializeField] Animator playerAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+
+        playerRigidbody.velocity = new Vector2(horizontalMovement, verticalMovement);
+
+        playerAnimator.SetFloat("movementX", playerRigidbody.velocity.x);
+        playerAnimator.SetFloat("movementY", playerRigidbody.velocity.y);
+    }
+}
